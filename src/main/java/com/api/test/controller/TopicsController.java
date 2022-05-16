d85994e401b45c9d1ae7cd347d9c11bf306806f4
JosielJunior <josiel-junior2@hotmail.com>
2022-05-16 17:13:29 -0300

    add atualizacao de topicos

--- src/main/java/com/api/test/controller/TopicsController.java
+++ src/main/java/com/api/test/controller/TopicsController.java
@@ -1,6 +1,7 @@
 package com.api.test.controller;
 
 import com.api.test.dto.DetalhesTopicoDto;
+import com.api.test.form.AtualizacaoTopicoForm;
 import com.api.test.form.TopicoForm;
 import com.api.test.dto.TopicoDto;
 import com.api.test.modelo.Topico;
@@ -8,6 +9,7 @@ import com.api.test.repository.CursoRepository;
 import com.api.test.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -48,6 +50,14 @@ public class TopicsController {
         Topico topico = topicoRepository.getById(id);
         return new DetalhesTopicoDto(topico);
 
+    }
+
+    @PutMapping("/{id}")
+    @Transactional
+    public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id,@RequestBody @Valid AtualizacaoTopicoForm form) {
+        Topico topico = form.atualizar(id, topicoRepository);
+
+        return ResponseEntity.ok(new TopicoDto(topico));
     }
 }
 
