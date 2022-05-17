5fb611efc8fad8221676119f2a0e51959394475a
Josiel Junior <64495134+josieljunior@users.noreply.github.com>
2022-05-16 22:40:26 -0300

    paginacao simplificada

--- src/main/java/com/api/test/controller/TopicsController.java
+++ src/main/java/com/api/test/controller/TopicsController.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -33,11 +34,8 @@ public class TopicsController {
     @GetMapping
     public Page<TopicoDto> lista(
             @RequestParam(required = false) String nomeCurso,
-            @RequestParam int pagina,
-            @RequestParam int qtd,
-            @RequestParam String ordenacao) {
+            @PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable paginacao) {
 
-        Pageable paginacao = PageRequest.of(pagina, qtd, Sort.Direction.DESC,ordenacao);
 
         if (nomeCurso == null){
             Page<Topico> topicos = topicoRepository.findAll(paginacao);
