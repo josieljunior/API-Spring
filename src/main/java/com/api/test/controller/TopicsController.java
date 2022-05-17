b51aa8b5c95dc45075dc918a28b3975b0391a555
Josiel Junior <64495134+josieljunior@users.noreply.github.com>
2022-05-16 22:28:17 -0300

    add ordenação

--- src/main/java/com/api/test/controller/TopicsController.java
+++ src/main/java/com/api/test/controller/TopicsController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -33,9 +34,10 @@ public class TopicsController {
     public Page<TopicoDto> lista(
             @RequestParam(required = false) String nomeCurso,
             @RequestParam int pagina,
-            @RequestParam int qtd) {
+            @RequestParam int qtd,
+            @RequestParam String ordenacao) {
 
-        Pageable paginacao = PageRequest.of(pagina, qtd);
+        Pageable paginacao = PageRequest.of(pagina, qtd, Sort.Direction.DESC,ordenacao);
 
         if (nomeCurso == null){
             Page<Topico> topicos = topicoRepository.findAll(paginacao);
