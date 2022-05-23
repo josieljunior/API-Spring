2e9d68e5d410394fe6428bbbfad2365618f38741
Josiel Junior <64495134+josieljunior@users.noreply.github.com>
2022-05-23 12:16:32 -0300

    add testes com mock-mvc

---
+++ src/test/java/com/api/test/controller/AutenticacaoControllerTest.java
@@ -0,0 +1,40 @@
+package com.api.test.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+@ActiveProfiles("test")
+class AutenticacaoControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void return400ForBadAuthentication() throws Exception {
+        URI uri = new URI("/auth");
+        String json ="{\"email\":\"invalido@email.com\",\"senha\":\"senha\"}";
+        mockMvc.perform(MockMvcRequestBuilders
+                .post(uri)
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status()
+                        .is(400));
+
+
+    }
+}
