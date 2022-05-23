53c1f2649164a297d371e03ecf9b1c7408e1fda3
Josiel Junior <64495134+josieljunior@users.noreply.github.com>
2022-05-23 11:42:25 -0300

    add db para testes

--- src/test/java/com/api/test/repository/CursoRepositoryTest.java
+++ src/test/java/com/api/test/repository/CursoRepositoryTest.java
@@ -5,19 +5,31 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
-//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 class CursoRepositoryTest {
 
     @Autowired
     private CursoRepository repository;
 
+    @Autowired
+    private TestEntityManager em;
+
     @Test
     public void showCourseByName(){
         String nomeCurso = "HTML 5";
+
+        Curso html5 = new Curso();
+        html5.setNome(nomeCurso);
+        html5.setCategoria("Programacao");
+        em.persist(html5);
+
         Curso curso = repository.findByNome(nomeCurso);
         assertNotNull(curso);
         assertEquals(nomeCurso, curso.getNome());
